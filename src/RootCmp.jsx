9a67f456e9002a3745b613cd6d31746a6958aac3
InbalMary <inbalby10@gmail.com>
2025-09-25 21:23:33 +0300

    Init repo and  add stay entity

---
+++ src/RootCmp.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+
+import { HomePage } from './pages/HomePage'
+import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
+import { CarIndex } from './pages/CarIndex.jsx'
+import { StayIndex } from './pages/StayIndex.jsx'
+import { ReviewIndex } from './pages/ReviewIndex.jsx'
+import { ChatApp } from './pages/Chat.jsx'
+import { AdminIndex } from './pages/AdminIndex.jsx'
+
+import { CarDetails } from './pages/CarDetails'
+import { StayDetails } from './pages/StayDetails'
+import { UserDetails } from './pages/UserDetails'
+
+import { AppHeader } from './cmps/AppHeader'
+import { AppFooter } from './cmps/AppFooter'
+import { UserMsg } from './cmps/UserMsg.jsx'
+import { LoginSignup, Login, Signup } from './pages/LoginSignup.jsx'
+
+
+export function RootCmp() {
+    return (
+        <div className="main-container">
+            <AppHeader />
+            <UserMsg />
+
+            <main>
+                <Routes>
+                    <Route path="" element={<HomePage />} />
+                    <Route path="about" element={<AboutUs />}>
+                        <Route path="team" element={<AboutTeam />} />
+                        <Route path="vision" element={<AboutVision />} />
+                    </Route>
+                    <Route path="car" element={<CarIndex />} />
+                    <Route path="car/:carId" element={<CarDetails />} />
+                    <Route path="stay" element={<StayIndex />} />
+                    <Route path="stay/:stayId" element={<StayDetails />} />
+                    <Route path="user/:id" element={<UserDetails />} />
+                    <Route path="review" element={<ReviewIndex />} />
+                    <Route path="chat" element={<ChatApp />} />
+                    <Route path="admin" element={<AdminIndex />} />
+                    <Route path="auth" element={<LoginSignup />}>
+                        <Route path="login" element={<Login />} />
+                        <Route path="signup" element={<Signup />} />
+                    </Route>
+                </Routes>
+            </main>
+            <AppFooter />
+        </div>
+    )
+}
+
+
