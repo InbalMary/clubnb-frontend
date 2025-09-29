0bf20da553a5a8a452f6b442dbb57b64c691646a
Michal-Om <michal_omri@yahoo.com>
2025-09-29 11:22:16 +0300

    Add demo stay data for local development

---
+++ src/data/demo-stays.js
@@ -0,0 +1,150 @@
+export const demoStays = [
+    {
+        "_id": "622f337a75c7d36e498aaaf8",
+        "name": "Westin Kaanapali KORVN 2BR",
+        "type": "National parks",
+        "imgUrls": [
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436975/hx9ravtjop3uqv4giupt.jpg",
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436294/mvhb3iazpiar6duvy9we.jpg"
+        ],
+        "price": 595,
+        "summary": "Westin Kaanapali Ocean Resort Villas North timeshare - ocean view and pools right outside your door.",
+        "capacity": 8,
+        "bathrooms": 2,
+        "bedrooms": 2,
+        "roomType": "Entire home/apt",
+        "startDate": "2025-10-15",
+        "endDate": "2025-10-20",
+        "host": {
+            "_id": "622f3403e36c59e6164faf93",
+            "fullname": "Patty And Beckett",
+            "location": "Eureka, California, United States",
+            "isSuperhost": true
+        },
+        "loc": {
+            "country": "United States",
+            "city": "Maui",
+            "address": "Lahaina, HI, United States"
+        },
+        "reviews": [],
+        "likedByUsers": []
+    },
+    {
+        "_id": "622f337a75c7d36e498aaaf9",
+        "name": "Belle chambre à côté Metro Papineau",
+        "type": "Campers",
+        "imgUrls": [
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663437045/dmquvficldi8ssfdlrrx.jpg",
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663437033/rhw6gycttaimzocc1poz.jpg"
+        ],
+        "price": 30,
+        "summary": "Private room in a modern apartment close to downtown Montreal with great access to public transport.",
+        "capacity": 2,
+        "bathrooms": 1,
+        "bedrooms": 1,
+        "roomType": "Private room",
+        "startDate": "2025-11-01",
+        "endDate": "2025-11-05",
+        "host": {
+            "_id": "622f3401e36c59e6164fabab",
+            "fullname": "Angel",
+            "location": "Montreal, Québec, Canada"
+        },
+        "loc": {
+            "country": "Canada",
+            "city": "Montreal",
+            "address": "Montréal, QC, Canada"
+        },
+        "reviews": [],
+        "likedByUsers": []
+    },
+    {
+        "_id": "622f337a75c7d36e498aaafa",
+        "name": "M&M Space MM2 - Central Apartment",
+        "type": "Campers",
+        "imgUrls": [
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436861/xrxhgsif3ekhxgn8irlm.jpg",
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663437017/gjyzgdjngyrhfrj2loxz.jpg"
+        ],
+        "price": 65,
+        "summary": "Beautifully located apartment near Porto's historic center and public transport.",
+        "capacity": 4,
+        "bathrooms": 1,
+        "bedrooms": 2,
+        "roomType": "Entire home/apt",
+        "startDate": "2025-09-25",
+        "endDate": "2025-09-30",
+        "host": {
+            "_id": "622f3403e36c59e6164fb266",
+            "fullname": "Maria",
+            "location": "Porto, Porto District, Portugal",
+            "isSuperhost": true
+        },
+        "loc": {
+            "country": "Portugal",
+            "city": "Porto",
+            "address": "Porto, Porto, Portugal"
+        },
+        "reviews": [],
+        "likedByUsers": []
+    },
+    {
+        "_id": "622f337a75c7d36e498aaafb",
+        "name": "Fresh and modern 1BR in Bed-Stuy",
+        "type": "National parks",
+        "imgUrls": [
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436912/xle8ueqxjeazbs4bp09p.jpg",
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436460/qi3vkpts37b4k0dedosc.jpg"
+        ],
+        "price": 79,
+        "summary": "Spacious, art-filled one-bedroom apartment close to Manhattan with great local cafes and shops.",
+        "capacity": 2,
+        "bathrooms": 1,
+        "bedrooms": 1,
+        "roomType": "Entire home/apt",
+        "startDate": "2025-12-10",
+        "endDate": "2025-12-15",
+        "host": {
+            "_id": "622f3402e36c59e6164fac46",
+            "fullname": "Shaila & Alex",
+            "location": "New York, United States",
+            "isSuperhost": true
+        },
+        "loc": {
+            "country": "United States",
+            "city": "New York",
+            "address": "Brooklyn, NY, United States"
+        },
+        "reviews": [],
+        "likedByUsers": []
+    },
+    {
+        "_id": "622f337a75c7d36e498aaafc",
+        "name": "Habitación centro de Barcelona",
+        "type": "Shared homes",
+        "imgUrls": [
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436983/pivldxmrxssnhyzixhes.jpg",
+            "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436394/kscsvxyn0uro9tjhefeb.jpg"
+        ],
+        "price": 40,
+        "summary": "Private room in the heart of Barcelona, near museums, shopping, and public transport.",
+        "capacity": 2,
+        "bathrooms": 1,
+        "bedrooms": 1,
+        "roomType": "Private room",
+        "startDate": "2025-11-20",
+        "endDate": "2025-11-23",
+        "host": {
+            "_id": "622f3407e36c59e6164fbdae",
+            "fullname": "Marián",
+            "location": "Barcelona, Catalonia, Spain"
+        },
+        "loc": {
+            "country": "Spain",
+            "city": "Barcelona",
+            "address": "Barcelona, Catalunya, Spain"
+        },
+        "reviews": [],
+        "likedByUsers": []
+    }
+]
