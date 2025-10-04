88a802fc5a4acede4f814b08b37e792d7a397666
InbalMary <inbalby10@gmail.com>
2025-10-04 21:19:41 +0300

    Make StayIndex be in the home page and remove stays and msgs links from header

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -71,12 +71,12 @@ export function RootCmp() {
 
             <main>
                 <Routes>
-                    <Route path="" element={<HomePage />} />
+                    {/* <Route path="" element={<HomePage />} />
                     <Route path="about" element={<AboutUs />}>
                         <Route path="team" element={<AboutTeam />} />
                         <Route path="vision" element={<AboutVision />} />
-                    </Route>
-                    <Route path="stay" element={<StayIndex />} />
+                    </Route> */}
+                    <Route path="" element={<StayIndex />} />
                     <Route path="stay/:stayId" element={<StayDetails />} />
                     <Route path="user/:id" element={<UserDetails />} />
                     <Route path="messages" element={<MsgIndex />} />
