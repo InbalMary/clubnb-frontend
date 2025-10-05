288ecd077727d688fbca24b251d79135266e7aba
Michal-Om <michal_omri@yahoo.com>
2025-10-05 09:54:59 +0300

    Style initial appFooter with placeholders for language and currency selection

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,16 +1,26 @@
 import { useSelector } from 'react-redux'
+import { appHeaderSvg } from './Svgs'
 
-export function AppFooter() {
-	const count = useSelector(storeState => storeState.userModule.count)
+export function AppFooter() { //TODO: accept language & currency props to update footer with user selection
+
 
 	return (
 		<footer className="app-footer full">
-			<p>Coffeerights &copy;  </p>
-			<p>Count: {count}</p>
-            
-            {import.meta.env.VITE_LOCAL ? 
-                <span className="local-services">Local Services</span> : 
-                <span className="remote-services">Remote Services</span>}
+			<p>&copy; Clubnb, Inc.</p>
+			<div className='footer-right'>
+				<div className='footer-language-group' onClick={() => console.log('TODO: open language selector')}>
+					<span className="globe-icon">{appHeaderSvg.changeLanguage}</span>
+					<span className='footer-language'>English (US)</span>
+				</div>
+
+				<div className='footer-currency-group' onClick={() => console.log('TODO: open currency selector')}>
+					<span className='footer-currency'>$</span>
+					<span className='footer-currency-name'>USD</span>
+				</div>
+			</div>
+			{/* {import.meta.env.VITE_LOCAL ?
+				<span className="local-services">Local Services</span> :
+				<span className="remote-services">Remote Services</span>} */}
 		</footer>
 	)
 }
