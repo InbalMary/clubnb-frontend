c1d1dd7f8de9080a4a492ba0f56283bb55811273
Michal-Om <michal_omri@yahoo.com>
2025-10-08 17:25:37 +0300

    Style left column and custom radio buttons in Confirm & Pay page

--- src/pages/ConfirmPay.jsx
+++ src/pages/ConfirmPay.jsx
@@ -16,6 +16,8 @@ export function ConfirmPay() { //later send order as a prop from a parent
     const stay = order.stay
     const totalFees = order.cleaningFee + order.serviceFee
     const totalPrice = order.totalPrice
+    const payNow = +(totalPrice * 0.2).toFixed(2)
+    const payLater = +(totalPrice - payNow).toFixed(2)
     const pricePerNight = order.pricePerNight
     const isGuestFavorite = stay.isGuestFavorite
     const ifIsRareFind = stay.isRareFind
@@ -38,84 +40,120 @@ export function ConfirmPay() { //later send order as a prop from a parent
                 <div className="confirm-layout">
                     {/* LEFT: main flow */}
                     <div className="confirm-main">
-
-
+                        {/*TODO: add conditional box shadow to each stage of confirmation*/}
                         <div className="payment-timing">
-                            <h3 className="section-title">Choose when to pay</h3>
-                            {/* payment timing options go here */}
+                            <div className='payment-options'>
+                                <h3 className="section-title">1. Choose when to pay</h3>
+                                <label className="payment-timing-option payment-radio">
+                                    <span>Pay ${order.totalPrice + totalFees} now</span>
+                                    <input type="radio" name="payment" value="full" />
+                                    <span className='payment-checkmark'></span>
+                                </label>
+
+                                <label className="payment-timing-option split-option payment-radio">
+                                    <div className='option-top'>
+                                        <span>Pay part now, part later</span>
+                                        <input type="radio" name="payment" value="split" />
+                                        <span className='payment-checkmark'></span>
+                                    </div>
+
+                                    <p className='split-pay'>
+                                        ${payNow} now, ${payLater} charged on [date TBD]. No extra fees.  {/*TODO: decide how many days before startDate to charge the second payment*/}
+                                        <button className='btn btn-link more-info'>More info</button>
+                                    </p>
+                                </label>
+                            </div>
+                            <button className='btn btn-black confirm-next'>Next</button>
                         </div>
 
                         <div className="payment-method">
-                            <h3 className="section-title">Add payment method</h3>
+                            <h3 className="section-title">2. Add payment method</h3>
                             {/* credit card / PayPal / etc. */}
                         </div>
                         <div className="host-message">
-                            <h3 className="section-title">Message the host</h3>
+                            <h3 className="section-title">3. Message the host</h3>
                             {/* textarea for message */}
                         </div>
                         <div className="reservation-review">
-                            <h3 className="section-title">Review your reservation</h3>
+                            <h3 className="section-title">4. Review your reservation</h3>
                             {/* reservation summary */}
                         </div>
                     </div>
-                    {/* RIGHT: summary card */}
-                    <aside className="reservation-summary">
-                        <div className='reservation-summary-order'>
-                            <img src={stay.imgUrl} alt="Order preview" className='reservation-summary-img' />
-                            <div className="reservation-summary-info">
-                                <h3 className='reservation-summary-title'>{stay.name}</h3>
 
-                                <div className="reservation-summary-details">
-                                    <span className='reservation-summary-rating'>
-                                        <span className='reservation-summary-star'>{statSvgs.starSmall}</span>
-                                        {stay.rating} ({stay.numReviews})
-                                    </span>
-                                    {isGuestFavorite && (
-                                        <span className='reservation-summary-badge'>
-                                            <span className='guest-favorite-icon'>{badgesSvgs.guestFavorite}</span>Guest favorite</span>
-                                    )}
+
+                    <div className='reservation-summary-wrapper'>
+                        {/* RIGHT: summary card */}
+                        <aside className="reservation-summary">
+                            <div className='reservation-summary-order'>
+                                <img src={stay.imgUrl} alt="Order preview" className='reservation-summary-img' />
+                                <div className="reservation-summary-info">
+                                    <h3 className='reservation-summary-title'>{stay.name}</h3>
+                                    <div className="reservation-summary-details">
+                                        <span className='reservation-summary-rating'>
+                                            <span className='reservation-summary-star'>{statSvgs.starSmall}</span>
+                                            {stay.rating} ({stay.numReviews})
+                                        </span>
+                                        {isGuestFavorite && (
+                                            <span className='reservation-summary-badge'>
+                                                <span className='guest-favorite-icon'>{badgesSvgs.guestFavorite}</span>Guest favorite</span>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='cancellation-policy'>
-                            <p>Free cancellation</p>
-                            <p>Cancel before [date TBD] for a full refund</p> {/*Implement this function*/}
-                            <button className='btn btn-link'>Full policy</button>
-                        </div>
-                        <div className='reservation-summary-dates'>
-                            <p>Dates</p>
-                            <p>{formatStayDates(order.startDate, order.endDate)}, {year}</p> {/*TODO: find year of order */}
-                        </div>
-                        <div className='reservation-summary-guests'>
-                            <p>Guests</p>
-                            <p>{adults} adults</p>
-                            {children > 0 && <p>{children} {children === 1 ? 'child' : 'children'}</p>}
-                            {infants > 0 && <p>{infants} {infants === 1 ? 'infant' : 'infants'}</p>}
-                        </div>
-                        <div className='reservation-price'>
-                            {/* TODO: replace static values with dynamic calculation */}
-                            <p>Price details</p>
-                            <p>{order.numNights} X ${pricePerNight} <span>${totalPrice}</span></p>
-                            <p>Cleaning fee <span>${order.cleaningFee}</span></p>
-                            <p>Clubnb service fee <span>${order.serviceFee}</span></p>
-                        </div>
-                        <div className='reservation-summary-total'>
-                            <p>Total
-                                <span className='reservation-currency'> USD</span> {/*TODO: change later to a button link for dynamic currency change options modal */}
-                                <span className='reservation-final-total'>${order.totalPrice + totalFees}</span>
-                            </p>
-                            <button className='btn btn-link price-breakdown'>Price breakdown</button> {/*TODO: later add modal onClick */}
-                        </div>
+                            <div className='cancellation-policy'>
+                                <p>Free cancellation</p>
+                                <p>Cancel before [date TBD] for a full refund.
+                                    <button className='btn btn-link'>Full policy</button>
+                                </p> {/*Implement this function*/}
+
+                            </div>
+                            <div className='reservation-summary-dates'>
+                                <p>Dates</p>
+                                <p>{formatStayDates(order.startDate, order.endDate)}, {year}</p>
+                            </div>
+                            <div className='reservation-summary-guests'>
+                                <p>Guests</p>
+                                <p>{adults} adults</p>
+                                {children > 0 && <p>{children} {children === 1 ? 'child' : 'children'}</p>}
+                                {infants > 0 && <p>{infants} {infants === 1 ? 'infant' : 'infants'}</p>}
+                            </div>
+                            <div className='reservation-price'>
+                                {/* TODO: replace static values with dynamic calculation */}
+                                <p>Price details</p>
+                                <div className="price-details">
+                                    <div className='reservation-total-nights'>
+                                        <p>{order.numNights} nights x ${pricePerNight}</p>
+                                        <span>${totalPrice}</span>
+                                    </div>
+                                    <div className='cleaning-fee'>
+                                        <p>Cleaning fee</p>
+                                        <span>${order.cleaningFee}</span>
+                                    </div>
+                                    <div className='service-fee'>
+                                        <p>Clubnb service fee</p>
+                                        <span>${order.serviceFee}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='reservation-summary-total'>
+                                <p>Total</p>
+                                <div className='total-amount'>
+                                    <span className='reservation-currency'> USD</span> {/*TODO: change later to a button link for dynamic currency change options modal */}
+                                    <span className='reservation-final-total'>${order.totalPrice + totalFees}</span>
+                                </div>
+                                <button className='btn btn-link price-breakdown'>Price breakdown</button> {/*TODO: later add modal onClick */}
+                            </div>
+                        </aside>
                         {ifIsRareFind && (
-                            <div>
-                                <p>
-                                    <span className='rare-find-icon'>{badgesSvgs.rareFind}</span>
-                                    This is a rare find</p>
-                                <p>{order.host.firstName}'s is usually booked</p>
+                            <div className='rare-find'>
+                                <span className='rare-find-icon'>{badgesSvgs.rareFind}</span>
+                                <div className='rare-find-text'>
+                                    <p>This is a rare find</p>
+                                    <p>{order.host.firstName}'s place is usually booked.</p>
+                                </div>
                             </div>
                         )}
-
-                    </aside>
+                    </div>
                 </div>
             </section>
         </>
