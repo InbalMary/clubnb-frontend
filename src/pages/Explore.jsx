ebb8fdd90b36226fc0f337bb6cd03cb3a6a67086
Yulia C <yulka1355@gmail.com>
2025-10-16 19:17:19 +0300

    Add  map to explore page.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,12 +3,14 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { loadStays, setFilterBy } from '../store/actions/stay.actions'
 import { StayPreview } from '../cmps/StayPreview'
+import { ExploreMap } from '../cmps/ExploreMap'
 
 export function Explore() {
     const { city, type } = useParams()
     const { stays, isLoading } = useSelector(storeState => storeState.stayModule)
     // console.log('Explore render triggered', { type, stays, isLoading })
     useEffect(() => {
+        console.log('Explore type param:', type)
         if (type) {
             setFilterBy({ type })
             loadStays({ type })
@@ -22,20 +24,24 @@ export function Explore() {
 
 
     return (
-        <section className="explore-page main-container">
+        // main-container
+        <section className="explore-page ">
+            <div className="items-wrapper">
 
-            {isLoading ? (
-                <h4 className='explore-title'>Loading homes...</h4>
-            ) : (
-                <h4 className='explore-title'>Over {stays?.length - 1} homes</h4>
-            )}
-            {/* grid of stays */}
-            <div className="explore-grid">
-                {stays?.map(stay => (
-                    <StayPreview key={stay._id} stay={stay} isBig={true} />
+                {isLoading ? (
+                    <h4 className='explore-title'>Loading homes...</h4>
+                ) : (
+                    <h4 className='explore-title'>Over {stays?.length - 1} homes</h4>
+                )}
+                {/* grid of stays */}
+                <div className="explore-grid">
+                    {stays?.map(stay => (
+                        <StayPreview key={stay._id} stay={stay} isBig={true} />
 
-                ))}
+                    ))}
+                </div>
             </div>
+            <ExploreMap locations={stays} />
         </section>
 
 
