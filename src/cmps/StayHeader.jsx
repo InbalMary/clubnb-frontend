b2fbc486845920436814c388a0d2edaf1a450939
Yulia C <yulka1355@gmail.com>
2025-10-27 22:27:47 +0200

    Update stayHeader. Add mobile class and mobile layout.

--- src/cmps/StayHeader.jsx
+++ src/cmps/StayHeader.jsx
@@ -3,8 +3,9 @@ import { useIsShortPage, useSHowOnScroll } from "../customHooks/useShowOnScroll"
 import { MiniStickyContainer } from "./SmallComponents";
 import { setCurrentOrder } from "../store/actions/order.actions";
 import { calculateNights } from "../services/util.service";
+import { useIsBreakPoint } from "../customHooks/useIsBreakPoint";
 
-export function StayHeader({ refs, stay,onClick, startDate, endDate }) {
+export function StayHeader({ refs, stay, onClick, startDate, endDate }) {
     const { stayId } = useParams()
     const navigate = useNavigate()
 
@@ -14,57 +15,37 @@ export function StayHeader({ refs, stay,onClick, startDate, endDate }) {
 
     const stickyHeader = useSHowOnScroll(570)
     const stickyReserve = useSHowOnScroll(2300)
-    // const isShortPage = useIsShortPage(1200)
 
-    // function handleClick() {
-    //     const from = startDate
-    //     const to = endDate
-
-    //     if (from && to) {
-    //         const numNights = calculateNights(from, to)
-    //         const pricePerNight = stay.price
-    //         const cleaningFee = stay.cleaningFee || 0
-    //         const serviceFee = Math.round(pricePerNight * 0.1)
-
-    //         const order = {
-    //             stay,
-    //             host: stay.host,
-    //             startDate: from,
-    //             endDate: to,
-    //             guests,
-    //             numNights,
-    //             pricePerNight,
-    //             cleaningFee,
-    //             serviceFee,
-    //             totalPrice: numNights * pricePerNight
-    //         }
-    //         setCurrentOrder(order)
-    //         navigate(`/stay/${stay._id}/confirm-pay`, { state: { order } })
-    //     } else {
-    //         scrollToSection(refs.stickyContainerRef)
-    //     }
-    // }
+    const isWide = useIsBreakPoint(1130)
+    const isMobile = useIsBreakPoint(745)
 
     return (
-        <div className="details-header">
+        <>
+            {!isMobile ? (
+                <div className={`stay-header big-layout ${isWide ? 'mid-layout' : ''} ${isMobile && 'mobile'}`}>
+                    {stickyHeader &&
 
-            {stickyHeader &&
-                <div className="stay-header main-content full">
-                    <nav className='nav-bar'>
-                        <span className="span-nav" onClick={() => scrollToSection(refs.photoRef)}>Photos <div className="border-bot"></div></span>
-                        <span className="span-nav" onClick={() => scrollToSection(refs.amenitiesRef)}>Amenities <div className="border-bot"></div></span>
-                        <span className="span-nav" onClick={() => scrollToSection(refs.reviewRef)}>Reviews <div className="border-bot"></div></span>
-                        <span className="span-nav" onClick={() => scrollToSection(refs.locationRef)}>Location <div className="border-bot"></div></span>
+                        <nav className='nav-bar'>
+                            <span className="span-nav" onClick={() => scrollToSection(refs.photoRef)}>Photos <div className="border-bot"></div></span>
+                            <span className="span-nav" onClick={() => scrollToSection(refs.amenitiesRef)}>Amenities <div className="border-bot"></div></span>
+                            <span className="span-nav" onClick={() => scrollToSection(refs.reviewRef)}>Reviews <div className="border-bot"></div></span>
+                            <span className="span-nav" onClick={() => scrollToSection(refs.locationRef)}>Location <div className="border-bot"></div></span>
 
-                        {stickyReserve &&
-                            <MiniStickyContainer stay={stay} startDate={startDate} endDate={endDate} onClick={onClick} />
-                        }
-                    </nav>
-
+                            {stickyReserve &&
+                                <MiniStickyContainer stay={stay} startDate={startDate} endDate={endDate} onClick={onClick} />
+                            }
+                        </nav>
+                    }
                 </div>
-            }
-        </div>
+            ) : (
+                <div className={`stay-header big-layout ${isWide ? 'mid-layout' : ''} ${isMobile && 'mobile'}`}>
 
+                    <MiniStickyContainer stay={stay} startDate={startDate} endDate={endDate} onClick={onClick} />
+                </div>
+
+            )}
+
+        </>
 
     )
 }
